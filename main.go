35350b0e600e5b84057dd30a30c7c6865ec59144
Clément Joly <l@131719.xyz>
2021-04-10 22:50:56 +0100

    initial attempt, does not work because mosh requires a terminal closer to the vanilla one

--- main.go
+++ main.go
@@ -18,8 +18,118 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type ConnectionType int
+
+const (
+	Mosh ConnectionType = iota
+	Ssh  ConnectionType = iota
+)
+
+func help(args []string) {
+	fmt.Printf("Mosh SRV\n%s hostname [mosh arguments...]\n\nThe hostname argument is required.\n", args[0])
+	os.Exit(255)
+}
+
+func querySRV(connection ConnectionType, hostname string) (srvs []*net.SRV, err error) {
+	switch connection {
+	case Mosh:
+		_, addrs, err := net.LookupSRV("mosh", "udp", hostname)
+		if err != nil {
+			return srvs, err
+		}
+		return addrs, nil
+	case Ssh:
+		net.LookupSRV("ssh", "tcp", hostname)
+		log.Fatalln("SSH connection is not implemented yet")
+		return srvs, err
+	default:
+		log.Fatalln("Unknown connection type")
+		return srvs, err
+	}
+}
+
+// callShell calls the remote shell
+func callShell(connection ConnectionType, srv *net.SRV, shellArgs []string) error {
+	shell := ""
+	switch connection {
+	case Mosh:
+		shell = "mosh"
+		// shell = "ssh"
+	case Ssh:
+		shell = "ssh"
+	default:
+		log.Fatalln("Unknown connection type")
+	}
+
+	// Remove the last dot in the Target
+	var b strings.Builder
+	if last := len(srv.Target) - 1; srv.Target[last] == '.' {
+		b.WriteString(srv.Target[:last])
+	} else {
+		b.WriteString(srv.Target)
+	}
+	hostname := b.String()
+
+	args := []string{}
+	args = append(args, shellArgs...)
+	// -p is used by both SSH and Mosh to set port
+	args = append(args, "-p", fmt.Sprintf("%v:%v", srv.Port, srv.Port+1000))
+	// args = append(args, "-p", fmt.Sprintf("%v", srv.Port))
+	args = append(args, "--no-ssh-pty")
+	args = append(args, hostname)
+
+	fmt.Println("args", args)
+
+	cmd := exec.Command(shell, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	log.Printf("execute %#v\n", cmd)
+	log.Printf("EXECUTE %v\n", cmd)
+	return cmd.Run()
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	args := os.Args
+	if len(args) < 2 {
+		help(args)
+	}
+
+	hostname := args[1]
+	connection := Mosh
+	shellArgs := make([]string, len(args)-2)
+	if len(args) > 2 {
+		copy(shellArgs, args[2:])
+	}
+
+	fmt.Println("args", args)
+
+	srvs, err := querySRV(connection, hostname)
+	if err != nil {
+		log.Fatalln("Error querying SRV records:", err)
+	}
+
+	fmt.Println("SRVs", srvs)
+
+	for _, srv := range srvs {
+		fmt.Println("Trying host", srv.Target)
+		err := callShell(connection, srv, shellArgs)
+		if err != nil {
+			if err, ok := err.(*exec.ExitError); ok {
+				log.Println("INSIDE", err)
+				continue
+			}
+			log.Fatal(err)
+		}
+	}
 }
